70407cf4934bc0b2625af5d8ef556187735702da
Ghaith AlGhaith <gh4ith@gmail.com>
2019-10-16 00:59:38 +0300

    Adding Python AX.25 Receive over TCP

---
+++ AX25/receive_kiss_frame.py
@@ -0,0 +1,101 @@
+import hexdump
+import struct
+
+def decode_addr(data, cursor):
+    (a1, a2, a3, a4, a5, a6, a7) = struct.unpack("<BBBBBBB", data[cursor:cursor+7])
+    hrr = a7 >> 5
+    ssid = (a7 >> 1) & 0xf     
+    ext = a7 & 0x1
+
+    addr = struct.pack("<BBBBBB", a1 >> 1, a2 >> 1, a3 >> 1, a4 >> 1, a5 >> 1, a6 >> 1)
+    if ssid != 0:
+      call = "{}-{}".format(addr.strip(), ssid)
+    else:
+      call = addr
+    return (call, hrr, ext)
+
+def decode_uframe(ctrl, data, pos):
+    print ("U Frame")
+    if ctrl == 0x3:
+        # UI frame
+        (pid,) = struct.unpack("<B", data[pos])
+        pos += 1
+        rem = len(data[pos:-2])
+        info = struct.unpack("<" + "B"*rem, data[pos:-2])
+        pos += rem
+        fcs = struct.unpack("<BB", data[pos:])
+        print ("INFO: " + struct.pack("<" + "B"*len(info), *info))
+         
+
+def decode_sframe(ctrl, data, pos):
+    print ("S Frame")
+
+def decode_iframe(ctrl, data, pos):
+    print ("I Frame")
+
+def p(frame):
+    pos = 0
+    what_is_this = struct.unpack("<B", bytes([frame[pos]]))
+    pos += 1
+
+    # DST
+    (dest_addr, dest_hrr, dest_ext) = decode_addr(frame, pos)
+    pos += 7
+    print(dest_addr)
+    print ("DST: " + dest_addr)
+    
+    # SRC
+    (src_addr, src_hrr, src_ext) = decode_addr(frame, pos)  
+    pos += 7
+    print ("SRC: " + src_addr)
+    
+    # REPEATERS
+    ext = src_ext
+    while ext == 0:
+        rpt_addr, rpt_hrr, ext = decode_addr(frame, pos)
+        print ("RPT: " + rpt_addr)
+        pos += 7
+
+    # CTRL
+    (ctrl,) = struct.unpack("<B", frame[pos])
+    pos += 1
+  
+    print (ctrl)
+
+    if (ctrl & 0x3) == 0x3:
+        decode_uframe(ctrl, frame, pos)
+    elif (ctrl & 0x3) == 0x1:
+        decode_sframe(ctrl, frame, pos)
+    elif (ctrl & 0x1) == 0x0:
+        decode_iframe(ctrl, frame, pos)
+
+    print(hexdump.hexdump(frame))
+
+
+
+import time
+import socket
+
+# creating a socket object
+s = socket.socket(socket.AF_INET,
+                  socket.SOCK_STREAM)
+
+# get local Host machine name
+host = '' # or just use (host == '')
+port = 8002
+
+# bind to pot
+s.bind((host, port))
+
+# Que up to 5 requests
+s.listen(1)
+
+while True:
+    # establish connection
+    clientSocket, addr = s.accept()
+    print("got a connection from %s" % str(addr))
+    data = clientSocket.recv(1024)
+    print(data)
+    p(data)
+    clientSocket.close()
+
